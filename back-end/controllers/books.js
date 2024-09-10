68294dcdae6622a9ae2000ac1e588fbe7a9b4f9e
Masson <morgan.masson006@gmail.com>
2024-09-10 16:30:57 +0200

    ajout best ratings

--- back-end/controllers/books.js
+++ back-end/controllers/books.js
@@ -36,9 +36,9 @@ exports.modifyBook = (req, res, next) => {
       .catch((error) => {
           res.status(400).json({ error });
       });
-  }
+}
 
-  exports.deleteBook = (req, res, next) => {
+exports.deleteBook = (req, res, next) => {
     Book.findOne({ _id: req.params.id})
     .then(book => {
         if (book.userId != req.auth.userId) {
@@ -55,24 +55,30 @@ exports.modifyBook = (req, res, next) => {
     .catch( error => {
         res.status(500).json({ error });
     });
-  }
+}
 
-  exports.getOneBook = (req, res, next) => {
+exports.getOneBook = (req, res, next) => {
     Book.findOne({ _id: req.params.id })
       .then(book => res.status(200).json(book))
       .catch(error => res.status(404).json({ error }));
-  }
+}
 
-  exports.getAllBooks = (req, res, next) => {
+exports.getAllBooks = (req, res, next) => {
     Book.find()
         .then(book => res.status(200).json(book))
         .catch(error => res.status(400).json({error}))
-  }
+}
 
-  exports.rating = (req, res, next) =>{
+exports.rating = (req, res, next) =>{
 
-  }
+}
 
-  exports.getBestRating = (req, res, next) =>{
-    
-  }
+exports.getBestRating = (req, res, next) =>{
+    console.log("Requête reçue sur /bestrating");
+    Book.find().sort({ averageRating: -1 }).limit(3)
+    .then(books => {
+        res.status(200).json(books);
+    }).catch(error => {
+        res.status(400).json({ error });
+    });
+}
--- back-end/routes/books.js
+++ back-end/routes/books.js
@@ -4,12 +4,14 @@ const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 const bookctrl = require('../controllers/books');
 
+router.get('/bestrating', bookctrl.getBestRating);
+
 router.post('/', auth, multer, bookctrl.createBook);
 router.put('/:id', auth, multer, bookctrl.modifyBook);
 router.delete('/:id', auth, bookctrl.deleteBook);
-router.get('/:id', bookctrl.getOneBook);
-router.get('/',  bookctrl.getAllBooks);
 router.get('/:id/rating',auth, bookctrl.rating );
-router.get('/bestrating', auth, bookctrl.getBestRating);
+router.get('/:id', bookctrl.getOneBook);
+
+router.get('/',  bookctrl.getAllBooks);
 
 module.exports = router;
