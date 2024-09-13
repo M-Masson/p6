12a50fd1fb6fb3d655b65e017f6152c8d74ef364
Masson <morgan.masson006@gmail.com>
2024-09-13 16:01:39 +0200

    ajout sharp

--- back-end/middleware/multer-config.js
+++ back-end/middleware/multer-config.js
@@ -1,19 +1,46 @@
 const multer = require('multer');
-
-const MIME_TYPES = {
-  'image/jpg': 'jpg',
-  'image/jpeg': 'jpg',
-  'image/png': 'png'
-};
+const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path');
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, 'images');
   },
   filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
+    callback(null, Date.now() + '-' + file.originalname);
   }
 });
-module.exports = multer({storage: storage}).single('image');
+const upload = multer({storage}).single('image')
+
+module.exports =  (req, res, next)=>{
+  upload(req, res, async(error)=>{
+    if (error){
+      return res.status(500).json({error: error.message});
+    }
+    if (req.file){
+      const inputPath = req.file.path;
+      const outputFileName = Date.now()+'-'+path.parse(req.file.originalname).name+ '.webp'
+      const outputPath = path.join('images', outputFileName)
+
+      try{
+        await sharp(inputPath)
+        .resize({width: 200})
+        .toFormat('webp')
+        .webp({quality: 80})
+        .toFile(outputPath)
+
+        await fs.promises.unlink(inputPath)
+
+        req.file.path = outputPath
+        req.file.filename = outputFileName
+        next() 
+      }
+      catch (error){
+        res.status(500).json({error})
+      }
+    }else{
+      next()
+    }
+  })
+}
